0bae48d8d2c038cdef82da6eb827e3b0c10df34f
KubaG2137 <kubagarncarek@gmail.com>
2023-07-20 18:55:10 +0200

    Second part of day 1 challenge

--- src/main/java/Advent_of_code_2022/Day1/Day1.java
+++ src/main/java/Advent_of_code_2022/Day1/Day1.java
@@ -2,18 +2,23 @@ package Advent_of_code_2022.Day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Day1 {
     public static void main(String[] args) throws FileNotFoundException {
 
-        File file = new File("..\\Advent_of_code\\src\\Advent_of_code_2022\\Day1\\input.txt");
+        File file = new File("..\\Advent_of_code\\src\\main\\java\\Advent_of_code_2022\\Day1\\input.txt");
         Scanner sc = new Scanner(file);
 
         int calories = 0;
         int maxCalories = 0;
         String current;
-
+        HashMap<String, Integer> elves = new HashMap<>();
+        int elfNum = 1;
         while (sc.hasNext()) {
             current = sc.nextLine();
 
@@ -22,6 +27,9 @@ public class Day1 {
 
             } else {
 
+                elves.put("elf " + elfNum, calories);
+                elfNum++;
+
                 if (calories > maxCalories) {
                     maxCalories = calories;
                 }
@@ -29,7 +37,19 @@ public class Day1 {
                 calories = 0;
             }
         }
-        System.out.println(maxCalories);
+        List<String> keys = elves.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue()
+                        .reversed()).limit(3)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        int topElvesTotal = elves.get(keys.get(0)) + elves.get(keys.get(1)) + elves.get(keys.get(2));
+
+        System.out.printf("""
+                        Max calories is: %d. First top 3 elves with biggest calories are:\s
+                        %s with %d, %s with %d and %s with %d.\s
+                        Total of top 3 elves is %d""",
+                maxCalories, keys.get(0), elves.get(keys.get(0)),
+                keys.get(1), elves.get(keys.get(1)), keys.get(2), elves.get(keys.get(2)), topElvesTotal);
     }
 }
-
